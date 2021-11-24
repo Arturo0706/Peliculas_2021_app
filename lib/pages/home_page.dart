bcd762be497fa56c78e4db73b382a4515d676d4c
DSM43 Arturo Romero Gonzalez <al222011149@gmail.com>
2021-11-23 21:26:06 -0600

    Petición de servidor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'package:card_swiper/card_swiper.dart';
 import 'package:movies/widgets/swiper_card_widget.dart';
+import 'package:movies/providers/movie_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = MoviesProvider();
+    moviesProvider.getNowPlaying();
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
